d27525131496bbe92834d1c76b1d84516ee6ab86
Pat DeSantis <pdesantis3@gmail.com>
2022-03-13 23:18:36 -0400

    Update project name

--- BGMDriver/BGMDriver/BGM_AbstractDevice.h
+++ BGMDriver/BGMDriver/BGM_AbstractDevice.h
@@ -1,17 +1,17 @@
-// This file is part of Background Music.
+// This file is part of Descript Loopback Driver.
 //
-// Background Music is free software: you can redistribute it and/or
+// Descript Loopback Driver is free software: you can redistribute it and/or
 // modify it under the terms of the GNU General Public License as
 // published by the Free Software Foundation, either version 2 of the
 // License, or (at your option) any later version.
 //
-// Background Music is distributed in the hope that it will be useful,
+// Descript Loopback Driver is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Background Music. If not, see <http://www.gnu.org/licenses/>.
+// along with Descript Loopback Driver. If not, see <http://www.gnu.org/licenses/>.
 
 //
 //  BGM_AbstractDevice.h
